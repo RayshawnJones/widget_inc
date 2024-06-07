2edc273c96a67b8d4124728c9ad2e0272012c279
R.Jones <RayshawnJones1991@gmail.com>
2024-06-07 11:48:02 -0400

    Created Delete.jsx file for Widget Container Folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
+import DeleteWidget from './WidgetContainer/Delete.jsx'
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/widgets/new" element={<WidgetCreate />} />
         <Route path="/widgets/:widgetId" element={<WidgetShow />} />
         <Route path="/widgets/:widgetId/edit" element={<WidgetUpdate />} />
+        <Route path="/widgets/:widgetId/delete" element={<DeleteWidget />} />
       </Routes>
     </>
   );
