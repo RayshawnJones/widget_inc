b6a15459ddbaea3df216269e1274346d28341efc
smkun <skunian@yahoo.com>
2024-06-07 16:52:55 -0400

    fixed home.jsx

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,15 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
-import './Home.css'
+import './Home.css';
 
-
-
-
-
-const Home = ({ widgetsArray }) => {
-    const [widgets, setWidgets] = useState('')
+const Home = () => {
+    const [widgets, setWidgets] = useState([]);
 
     useEffect(() => {
         axios.get('http://3.14.175.247:8000/api/widgets/')
@@ -25,7 +20,7 @@ const Home = ({ widgetsArray }) => {
         <div id='home-wrapper'>
             <h1>Welcome to Widgets, Inc.</h1>
             <ul>
-                {widgetsArray.map(widget => (
+                {widgets.map(widget => (
                     <li key={widget.id}>
                         <Link to={`/widgets/${widget.id}`}>{widget.name} - ${widget.price}</Link>
                     </li>
