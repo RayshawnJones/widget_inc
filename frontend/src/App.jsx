3a6017b0ab566b6c2700e03a55f5f9ce11dde5d9
izzylppz <https://github.com/Shmizzy>
2024-06-07 11:01:12 -0500

    update form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
+import WidgetUpdate from './components/WidgetContainer/Update/Update'
 
 const App = () => {
 
