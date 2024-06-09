717b1eca4d9c15f5960edc4680b6b87906f2da0e
smkun <skunian@yahoo.com>
2024-06-09 11:57:08 -0400

    fixed settings.py for aws

--- backend/widgetstore/widgetstore/settings.py
+++ backend/widgetstore/widgetstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k&-dfh_fk=d*33gv-89drfqzpvjurx95xf#z4j4($^#_2u^$kq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.14.175.247']
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
